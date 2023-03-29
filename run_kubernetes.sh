71dab1e480dc7a7a4787319218ab57b55dd3af0f
EC2 Default User <ec2-user@ip-172-31-3-84.ec2.internal>
2023-03-29 08:38:55 +0000

    Project4

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath="HimangiR1907/udacityproject4"
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run udacityproject4  --image=$dockerpath --port=80 --labels app=udacityproject4
 
-
+kubectl wait pod/udacitypr4 --for=condition=Ready --timeout=-1s
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacityproject4 8080:80
